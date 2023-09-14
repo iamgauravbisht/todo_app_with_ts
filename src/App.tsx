02de9a1a52e294efabdcae1d3de6e7c40c958734
Gaurav <gauravbisht154@gmail.com>
2023-09-15 02:15:45 +0530

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return <Button onClick={() => console.log("its working")}>Gaurav</Button>;
+}
+
+export default App;
