ec87277515d90d8924cf3bb21a2f070f9afb3472
Gaurav <gauravbisht154@gmail.com>
2023-09-15 14:08:30 +0530

    todo app completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,60 @@
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import { Input } from "./components/ui/input";
+// import { Label } from "./components/ui/label";
+import { Fragment, useState } from "react";
 
 function App() {
-  return <Button onClick={() => console.log("its working")}>Gaurav</Button>;
+  const [value, setValue] = useState<string>("");
+  const [list, setList] = useState<string[]>([]);
+
+  const listBuilder = () => {
+    setList([...list, value]);
+    setValue("");
+  };
+  const deleteList = (index: number) => {
+    list.splice(index, 1);
+    setList([...list]);
+  };
+  const updateValue = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    list[index] = e.target.value;
+    setList([...list]);
+  };
+
+  return (
+    <Fragment>
+      <h1>TO DO LIST</h1>
+      <div className="flex gap-2 justify-center mt-2">
+        <Input
+          type="text"
+          id="todo"
+          className="w-48 outline-dashed"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button onClick={listBuilder}>&#x2713;</Button>
+      </div>
+      {list.map((item, index) => {
+        return (
+          <div className="flex gap-2 justify-center mt-2">
+            <Input
+              type="text"
+              value={item}
+              key={index}
+              className="w-fit "
+              onChange={(e) => updateValue(e, index)}
+            />
+            <Button variant="destructive" onClick={() => deleteList(index)}>
+              <i className="fa fa-trash" />
+            </Button>
+          </div>
+        );
+      })}
+    </Fragment>
+  );
 }
 
 export default App;
